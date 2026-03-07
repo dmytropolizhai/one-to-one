2c51a75773b2519e8319be47a41b1d8021d7fab4
Dmytro <dmytropolizhai@gmail.com>
2026-03-07 12:17:08 +0200

    refactor(chat): Rename ChatScreen to ChatPage

--- src/app/(app)/chat/page.tsx
+++ src/app/(app)/chat/page.tsx
@@ -1,10 +1,10 @@
 import { MessageInput } from "./_components/message-input";
 import { MessageList } from "./components/message-list";
 
-export default function ChatScreen() {
+export default function ChatPage() {
   return (
     <main className="flex h-screen w-full flex-col items-center justify-start gap-4 bg-background p-4">
-      <h1 className="text-2xl font-bold">Chat Screen</h1>
+      <h1 className="text-2xl font-bold">Chat</h1>
       <section className="flex flex-col gap-2 w-full h-full justify-between p-4">
         <MessageList />
         <MessageInput />
