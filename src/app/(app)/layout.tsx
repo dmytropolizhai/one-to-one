7e66a3d1380907197eef70b9a2f5c777194b7752
Dmytro <dmytropolizhai@gmail.com>
2026-03-07 13:55:24 +0200

    feat(app-layout): Add Sidebar to layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,8 @@
 import { getMe } from "@/data/users/actions";
+import { SidebarProvider, SidebarTrigger } from "@/shared/components/ui/sidebar";
 import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
+import { AppSidebar } from "./_components/app-sidebar";
 
 export default async function AppLayout({ children }: PropsWithChildren) {
     const user = await getMe();
@@ -9,5 +11,11 @@ export default async function AppLayout({ children }: PropsWithChildren) {
         redirect("/new")
     }
 
-    return children;
+    return (
+        <SidebarProvider defaultOpen>
+            <AppSidebar />
+            <SidebarTrigger />
+            {children}
+        </SidebarProvider>
+    );
 }
