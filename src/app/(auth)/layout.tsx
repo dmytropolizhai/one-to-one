aab58974c32f0dfa0177722fd62c5ee62ca1a750
Dmytro <dmytropolizhai@gmail.com>
2026-03-07 09:45:10 +0200

    feat(auth-layout): Set position to top-center

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from "react";
 import Background from "@/shared/components/background";
+import { Toaster } from "@/shared/components/ui/sonner";
 
 export default function AuthLayout({ children }: Readonly<PropsWithChildren>) {
     return (
         <main className="flex flex-col w-full min-h-screen justify-center items-center gap-4 p-4">
             <Background />
             {children}
+            <Toaster position="top-center" />
         </main>
     )
 }
