1ca93b084cf508691b1ea2780acf388588392c0c
Dmytro <dmytropolizhai@gmail.com>
2026-03-07 12:05:14 +0200

    refactor(users): Add validation and user creation

--- src/data/users/actions.ts
+++ src/data/users/actions.ts
@@ -1,7 +1,49 @@
-import { z } from "zod";
+"use server"
 
-export const createUserSchema = z.object({
-    nickname: z.string().min(3, "Nickname is too short").max(20, "Nickname is too long"),
-});
+import { createUserSchema, CreateUserData } from "@/data/users/schema";
+import { prisma } from "@/shared/lib/prisma";
 
-export type CreateUserData = z.infer<typeof createUserSchema>;
+export type CreateUserState = {
+    success: boolean;
+    errors?: Partial<Record<keyof CreateUserData, string[]>>;
+    message?: string;
+};
+
+export async function createUserAction(
+    _prev: CreateUserState,
+    formData: FormData
+): Promise<CreateUserState> {
+    const raw = Object.fromEntries(formData.entries());
+    const parsed = createUserSchema.safeParse(raw);
+
+    if (!parsed.success) {
+        return {
+            success: false,
+            errors: parsed.error.flatten().fieldErrors,
+        };
+    }
+
+    // Validate if user already exists
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            "email": parsed.data.email,
+        }
+    });
+    if (existingUser) {
+        return { success: false, message: "User already exists." };
+    }
+
+    // Create user
+    const user = await prisma.user.create({
+        data: {
+            name: parsed.data.name,
+            email: parsed.data.email,
+        }
+    });
+
+    if (user) {
+        return { success: true, message: "User created successfully." };
+    }
+
+    return { success: false, message: "Failed to create user." };
+}
