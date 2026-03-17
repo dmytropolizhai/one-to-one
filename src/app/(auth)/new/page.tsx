d6f6977a31a0f205be9f5c129bda9b6278719637
Dmytro <dmytropolizhai@gmail.com>
2026-03-17 22:28:34 +0200

    feat(new-page): Add responsive layout for mobile

--- src/app/(auth)/new/page.tsx
+++ src/app/(auth)/new/page.tsx
@@ -6,12 +6,12 @@ import { Skeleton } from "@/shared/components/ui/skeleton";
 
 export default function NewMemberPage() {
     return (
-        <div className="flex w-full justify-center items-center flex-row gap-8">
-            <header className="flex flex-col">
+        <div className="flex flex-col sm:flex-row w-full justify-center items-center sm:gap-10 gap-4">
+            <header className="flex flex-col text-left gap-2">
                 <h1 className="text-3xl font-bold">Become a new member</h1>
                 <p className="text-muted-foreground">Enter your nickname to create an account</p>
             </header>
-            <div className="h-20">
+            <div className="h-20 hidden md:block">
                 <Separator orientation="vertical" className="h-full" />
             </div>
             <Suspense fallback={<Skeleton className="w-full h-10" />}>
